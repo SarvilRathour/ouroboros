35d2071f6df265a81503a7b7a4268320644f004e
Sarvil Rathour <ken.kaneki.260905@gmail.com>
2026-06-26 23:43:03 +0530

    Verification email with otp being sent

--- src/handlers/auth_login.rs
+++ src/handlers/auth_login.rs
@@ -7,13 +7,50 @@ use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use rand::Rng;
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, AsyncSmtpTransport, Tokio1Executor, AsyncTransport};
 use serde_json::{Value, json};
 use std::env;
+use std::fmt;
+#[derive(Debug)]
+pub enum EmailError {
+    SendFailed,
+    InvalidEmail
+}
+impl fmt::Display for EmailError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EmailError::SendFailed => write!(f, "SendFailed"),
+            EmailError::InvalidEmail => write!(f, "InvalidEmail"),
+        }
+    }
+}
+impl std::error::Error for EmailError {}
 fn generate_6_digit_code() -> String {
     let mut rng = rand::thread_rng();
     let number = rng.gen_range(0..=999999);
     format!("{:06}", number)
 }
+pub async fn send_verification_email(email:&str,code:&str)->Result<(),EmailError> {
+    let smtp_username = env::var("SMTP_EMAIL").unwrap_or_default();
+    let smtp_password = env::var("SMTP_PASSWORD").unwrap_or_default();
+    let email = Message::builder()
+        .from(smtp_username.parse().unwrap())
+        .to(email.parse().unwrap())
+        .subject("Verification Code")
+        .body(format!("Your verification code is: {}", code))
+        .unwrap();
+    let creds = Credentials::new(smtp_username, smtp_password);
+    let mailer: AsyncSmtpTransport<Tokio1Executor> = 
+        AsyncSmtpTransport::<Tokio1Executor>::relay("smtp.gmail.com").unwrap()
+        .credentials(creds)
+        .build();
+    mailer.send(email).await.map_err(|_| EmailError::SendFailed)?;
+    Ok(())
+    
+}
+
 pub async fn login(
     State(appstate): State<AppState>,
     Json(payload): Json<LoginUserRequest>,
@@ -48,8 +85,13 @@ pub async fn login(
             return Err(StatusCode::INTERNAL_SERVER_ERROR);
         }
     };
+    
+    if let Err(e) = send_verification_email(&user.email, &code).await {
+            eprintln!("Failed to send email: {}", e);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     Ok(Json(json!({
-        "message": "Challenge created successfully",
+        "message": "Challenge created successfully and verification email sent",
         "challenge": challenge.id,
         "code":challenge.code,
         "expires_at": challenge.expires_at,
